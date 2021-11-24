26834c677c07eed3700e49a3fe65fa09e61e8d56
mariaelisa492 <mariaelisa004@hotmail.com>
2021-11-24 17:07:09 -0500

    build actualizado

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,11 +10,11 @@ const About = () => {
     <div className='about center'>
       {name && (
         <h1>
-          Hola, soy <span className='about__name'>{name}.</span>
+          Hola, soy <span className='about__name'>{name}</span>
         </h1>
       )}
 
-      {role && <h2 className='about__role'>{role}.</h2>}
+      {role && <h2 className='about__role'>{role}</h2>}
       <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
